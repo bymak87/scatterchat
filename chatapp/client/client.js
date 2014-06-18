5d5128cc48c2aa6ad091a250413a5e1863ed2c9c
Amy Bakameyer <bymak87@gmail.com>
2014-06-18 12:00:29 -0500

    added scatapp folder for scattergories and changed chat layout

--- chatapp/client/client.js
+++ chatapp/client/client.js
@@ -2,9 +2,7 @@
 * Templates
 */
 
-Template.messages.messages = function() {
-  return Messages.find({}, { sort: { time: -1}});
-};
+
 
 // Template.messages.img_add = function() {
 //   var hash = CryptoJS.MD5(git_email);
@@ -13,13 +11,13 @@ Template.messages.messages = function() {
 
 Template.input.events = {
   'keydown input#message' : function (event) {
+        var d = new Date();
     if (event.which == 13) { // 13 is the enter key event
       if (Meteor.user())
         var name = Meteor.user().profile.name;
       else
         var name = 'Anonymous';
         var message = document.getElementById('message');
-        var d = new Date();
       if (message.value != '') {
         Messages.insert({
           name: name,
@@ -32,4 +30,14 @@ Template.input.events = {
       }
     }
   }
+};
+
+Template.messages.messages = function() {
+  return Messages.find({
+
+  }, { sort: {
+    time: -1
+  },
+  limit:15
+  });
 };
