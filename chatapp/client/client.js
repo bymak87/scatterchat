1bd4fc812f2abfc199e13e220eadd47a0e891f44
Amy Bakameyer <bymak87@gmail.com>
2014-06-17 21:46:52 -0500

    formatted time stamp on chat

--- chatapp/client/client.js
+++ chatapp/client/client.js
@@ -6,6 +6,11 @@ Template.messages.messages = function() {
   return Messages.find({}, { sort: { time: -1}});
 };
 
+// Template.messages.img_add = function() {
+//   var hash = CryptoJS.MD5(git_email);
+//   return img_add = "http://www.gravatar.com/avatar/" + hash;
+// };
+
 Template.input.events = {
   'keydown input#message' : function (event) {
     if (event.which == 13) { // 13 is the enter key event
@@ -14,12 +19,12 @@ Template.input.events = {
       else
         var name = 'Anonymous';
       var message = document.getElementById('message');
-
+      var d = new Date();
       if (message.value != '') {
         Messages.insert({
           name: name,
           message: message.value,
-          time: Date.now(),
+          time: d.toLocaleTimeString(),
         });
 
         document.getElementById('message').value = '';
